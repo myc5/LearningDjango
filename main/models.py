7a25e940c921e87f2dff73c8800fc83ead3f7a18
Orange <Orange@ORANGE-PC>
2024-01-13 21:24:04 +0100

    Add DB

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class ToDoList(models.Model):
+    name = models.CharField(max_length = 200) #always add the max_length if using CharField
+    
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    # add entries from ToDoList as FKs; cascade like in SQL means that children entries are deleted if parent entries are deleted
+    # ToDoList here is an object
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    text = models.CharField(max_length = 300)
+    complete = models.BooleanField()
+    
+    def __str__(self):
+        return self.text
